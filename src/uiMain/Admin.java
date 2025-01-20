f8f414769ba0016728783a02fc33db8cbba7db51
camimira445 <jmirandaag@unal.edu.co>
2025-01-19 20:10:43 -0500

    CONTINUE

--- src/uiMain/Admin.java
+++ src/uiMain/Admin.java
@@ -59,3 +59,151 @@ public class Admin {
 			}
 		} while (opcion != 6);
 	}
+	
+	/* CASE 1 MAIN: VER TODOS LOS VUELOS DISPONIBLES POR AEROLINEAS
+	MUESTRA UNA TABLA POR CADA AEROLINEA CON LOS VUELOS QUE SE TIENEN
+	DISPONIBLES, HACIENDO USO DEL generadorDeTablas. */
+	static void mostrarVuelosPorAerolineas() {
+		ArrayList<Aerolinea> aerolineasDisponibles = Aerolinea.getAerolineas();
+		for(Aerolinea i:aerolineasDisponibles){
+			for(Vuelo j: i.getVuelos()){
+				j.setEstaCompleto(j.getAeronave().cambiarEstado());
+			}
+		}
+		generadorDeTablas.mostrarTablaDeVuelosDisponiblesPorAerolineas(aerolineasDisponibles);
+	}
+
+	/* CASE 2 MAIN: GENERAR TIQUETE DE COMPRA DE VUELO
+	EL METODO PERMITE GENERAR UN TIQUETE DE COMPRA DE UN VUELO AL BUSCAR POR DESTINO O POR DESTINO Y FECHA
+	LUEGO DE ELEGIR UN VUELO SE TOMAN LOS DATOS DEL PASAJERO Y SE ELIGE UNA SILLA EN LA AERONAVE
+	AL FINAL SE IMPRIME UN RESUMEN DE LA COMPRA. */
+	static void generarTiquete() {
+		for(Aerolinea i: Aerolinea.getAerolineas()){
+			for(Vuelo j: i.getVuelos()){
+				j.setEstaCompleto(j.getAeronave().cambiarEstado());
+			}
+		}
+		System.out.println("Quieres buscar un vuelo por:");
+		System.out.println("1. Destino");
+		System.out.println("2. Destino y fecha");
+		System.out.println("3. Regresar");
+		int opcion = sc.nextInt();
+		while (opcion != 1 & opcion != 2 & opcion != 3) {
+			System.out.println("Por favor ingresa una opcion valida");
+			opcion = sc.nextInt();
+		}
+
+		if (opcion == 1) {
+			System.out.println("Por favor ingrese un destino:");
+			String destino_1 = sc.next();
+			boolean hayVuelos = consultarVuelosPorDestino(destino_1);
+			if (!hayVuelos) {
+				return;
+			}
+		} else if (opcion == 2) {
+			System.out.println("Por favor ingrese un destino");
+			String destino_2 = sc.next();
+			System.out.println("Por favor ingrese una fecha (dd-mm-aaaa):");
+			String fecha_2 = sc.next();
+			boolean hayVuelos = consultarVuelosPorDestinoYFecha(destino_2, fecha_2);
+			if (!hayVuelos) {
+				return;
+			}
+		} else {
+			return;
+		}
+		System.out.println();
+		System.out.println("Por favor ingrese el nombre de la aerolinea con la que desea viajar");
+		String nombre_aerolinea = sc.next();
+		Aerolinea aerolinea = Aerolinea.buscarAerolineaPorNombre(nombre_aerolinea);
+
+		while (aerolinea == null) {
+			System.out.println("Por favor ingrese un nombre valido");
+			nombre_aerolinea = sc.next();
+			aerolinea = Aerolinea.buscarAerolineaPorNombre(nombre_aerolinea);
+		}
+
+		System.out.println("Por favor ingrese el ID del vuelo que desea comprar");
+		int ID = sc.nextInt();
+		Vuelo vuelo = aerolinea.buscarVueloPorID(aerolinea.getVuelos(), ID);
+		while (vuelo == null) {
+			System.out.println("Por favor ingrese un ID valido");
+			ID = sc.nextInt();
+			vuelo = aerolinea.buscarVueloPorID(aerolinea.getVuelos(), ID);
+		}
+
+		double ID_tiquete = 100 + Math.random() * 900; // DEVUELVE UN NUMERO ALEATORIO DE 3 CIFRAS
+		while (Aerolinea.BuscarTiquete((int) ID_tiquete) != null) {
+			ID_tiquete = 100 + Math.random() * 900;
+		}
+
+		// SECUENCIA DE PASOS PARA ELEGIR UNA SILLA
+		System.out.println("Que tipo de silla desea comprar?");
+		Silla silla = elegirSilla(vuelo);
+		if (silla == null) {
+			System.out.println("Lo sentimos no se encuentran sillas disponibles con esas caracteristicas\n");
+			return;
+		}
+		Tiquete tiquete = new Tiquete((int) ID_tiquete, vuelo.getPrecio(), vuelo);
+		tiquete.setSilla(silla);
+
+		// TOMAR DATOS DEL PASAJERO
+		System.out.println("DATOS DEL PASAJERO:");
+		System.out.println("Ingrese el nombre:");
+		String nombre = sc.next();
+		System.out.println("Ingrese su edad:");
+		int edad = sc.nextInt();
+		System.out.println("Ingrese el numero de su pasaporte:");
+		String pasaporte = sc.next();
+		System.out.println("Ingrese un e-mail");
+		String correo = sc.next();
+
+		//SE CREA EL OBJETO PASAJERO Y SE LE ASIGNA AL TIQUETE GENERADO EN EL METODO
+		Pasajero pasajero = new Pasajero(pasaporte, nombre, tiquete, edad, correo);
+
+		// IMPRIME RESUMEN DE LA COMPRA
+		tiquete.asignarPrecio();
+		System.out.println(tiquete);
+
+	}
+
+	/* CASE 3 MAIN: AGREGAR ALOJAMIENTO EN EL DESTINO DEL VUELO COMPRADO
+	EL METODO PERMITE AGREGAR UN ALOJAMIENTO A UN TIQUETE COMPRADO PREVIAMENTE, VERIFICANDO QUE NO SE TENGA UN ALOJAMIENTO COMPRADO
+	NI SE QUIERA AGREGAR UN ALOJAMIENTO EN UNA LOCACION DISTINTA A LA DEL DESTINO ASOCIADO AL TIQUETE.
+	AL INGRESAR EL NOMBRE DEL ALOJAMIENTO QUE SE DESEA AGREGAR SE SOLICITA EL NUMERO DE DIAS QUE SE QUIERE QUEDAR
+	PARA RECALCULAR EL PRECIO DEL TIQUETE, Y AL FINAL MOSTRAR EL RESUMEN DE LA COMPRA. */
+	static void agregarAlojamiento() {
+		System.out.println("Deseas agregar un alojamiento a tu compra?");
+		System.out.println("Por favor ingresa el ID del tiquete que se genero al comprar su vuelo:");
+		int tiqueteID = sc.nextInt();
+		Tiquete tiquete_solicitado = Aerolinea.BuscarTiquete(tiqueteID);
+
+		if (tiquete_solicitado == null) {
+			System.out.println("Lo sentimos, no tenemos un tiquete identificado con ese ID");
+			System.out.println();
+		}else if(tiquete_solicitado.getAlojamiento() != null) {
+			System.out.println("El tiquete ya posee un alojamiento, si quiere cambiarlo hagalo desde la opcion 4.\n");
+			return;
+		} else {
+			String destino = tiquete_solicitado.getVuelo().getDestino();
+			boolean hayAlojamientos = mostrarAlojamientosPorUbicacion(destino); //ESTE METODO SE DETALLA MAS ABAJO
+			if (!hayAlojamientos) {
+				return;
+			}
+			System.out.println("Por favor ingresa el nombre del alojamiento que desea anadir a su compra:");
+			String alojamiento = sc.next();
+			Alojamiento alojamiento_solicitado = Alojamiento.buscarAlojamientoPorNombre(alojamiento);
+			if (alojamiento_solicitado == null) {
+				System.out.println("Lo sentimos, no tenemos un alojamiento con ese nombre");
+				System.out.println();
+			}else if(!alojamiento_solicitado.getLocacion().equalsIgnoreCase(destino) ){
+				System.out.println("Lo sentimos, no tenemos un alojamiento con ese nombre en esa locacion\n");
+				return; }
+			else {
+				System.out.println("Cuantos dias desea quedarse en el alojamiento?");
+				int num_dias = sc.nextInt();
+				tiquete_solicitado.setAlojamiento(alojamiento_solicitado);
+				tiquete_solicitado.asignarPrecio(num_dias);
+
+				System.out.println("Perfecto! el alojamiento " + alojamiento_solicitado.getNombre()
+						+ " se ha agregado correctamente a su tiquete de compra.");
